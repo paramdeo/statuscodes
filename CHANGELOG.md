10ecb5029417f7826e671d0dd30e637da516faf8
Paramdeo Singh <paramdeo@paramdeo.com>
2021-10-26 14:54:57 -0400

    Updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to this project will be documented in this file (Added, Changed, Removed, Fixed).
 
+## [1.0.1] - 2021-10-26
+
+### Changed
+
+- Moved the info page from the `about.statuscodes.org` subdomain to the primary `statuscodes.org` domain to as to avoid them having to remove the subdomain before immediately testing
+
 ## [1.0.0] - 2021-09-18
 
 ### Added
--- index.js
+++ index.js
@@ -1,87 +1,97 @@
 async function handleRequest(request) {
-    const requestURL = new URL(request.url);
-    // Isolate & sanitize the path
-    const requestPath = requestURL.pathname.substring(1);
+  const requestURL = new URL(request.url);
+
+  // Isolate & sanitize the path
+  const requestPath = requestURL.pathname.substring(1);
+
+  const httpStatuses = [
+    "100 Continue",
+    "101 Switching Protocols",
+    "102 Processing",
+    "200 OK",
+    "201 Created",
+    "202 Accepted",
+    "203 Non-authoritative Information",
+    "204 No Content",
+    "205 Reset Content",
+    "206 Partial Content",
+    "207 Multi-Status",
+    "208 Already Reported",
+    "226 IM Used",
+    "300 Multiple Choices",
+    "301 Moved Permanently",
+    "302 Found",
+    "303 See Other",
+    "304 Not Modified",
+    "305 Use Proxy",
+    "307 Temporary Redirect",
+    "308 Permanent Redirect",
+    "400 Bad Request",
+    "401 Unauthorized",
+    "402 Payment Required",
+    "403 Forbidden",
+    "404 Not Found",
+    "405 Method Not Allowed",
+    "406 Not Acceptable",
+    "407 Proxy Authentication Required",
+    "408 Request Timeout",
+    "409 Conflict",
+    "410 Gone",
+    "411 Length Required",
+    "412 Precondition Failed",
+    "413 Payload Too Large",
+    "414 Request-URI Too Long",
+    "415 Unsupported Media Type",
+    "416 Requested Range Not Satisfiable",
+    "417 Expectation Failed",
+    "418 I'm a teapot",
+    "421 Misdirected Request",
+    "422 Unprocessable Entity",
+    "423 Locked",
+    "424 Failed Dependency",
+    "426 Upgrade Required",
+    "428 Precondition Required",
+    "429 Too Many Requests",
+    "431 Request Header Fields Too Large",
+    "444 Connection Closed Without Response",
+    "451 Unavailable For Legal Reasons",
+    "499 Client Closed Request",
+    "500 Internal Server Error",
+    "501 Not Implemented",
+    "502 Bad Gateway",
+    "503 Service Unavailable",
+    "504 Gateway Timeout",
+    "505 HTTP Version Not Supported",
+    "506 Variant Also Negotiates",
+    "507 Insufficient Storage",
+    "508 Loop Detected",
+    "510 Not Extended",
+    "511 Network Authentication Required",
+    "599 Network Connect Timeout Error"
+]
+
+  // Filter array using the startsWith() method to find a matching HTTP Status Code
+  const foundStatus = httpStatuses.filter(httpStatus => httpStatus.startsWith(requestPath));
   
-    const httpStatuses = [
-      "100 Continue",
-      "101 Switching Protocols",
-      "102 Processing",
-      "200 OK",
-      "201 Created",
-      "202 Accepted",
-      "203 Non-authoritative Information",
-      "204 No Content",
-      "205 Reset Content",
-      "206 Partial Content",
-      "207 Multi-Status",
-      "208 Already Reported",
-      "226 IM Used",
-      "300 Multiple Choices",
-      "301 Moved Permanently",
-      "302 Found",
-      "303 See Other",
-      "304 Not Modified",
-      "305 Use Proxy",
-      "307 Temporary Redirect",
-      "308 Permanent Redirect",
-      "400 Bad Request",
-      "401 Unauthorized",
-      "402 Payment Required",
-      "403 Forbidden",
-      "404 Not Found",
-      "405 Method Not Allowed",
-      "406 Not Acceptable",
-      "407 Proxy Authentication Required",
-      "408 Request Timeout",
-      "409 Conflict",
-      "410 Gone",
-      "411 Length Required",
-      "412 Precondition Failed",
-      "413 Payload Too Large",
-      "414 Request-URI Too Long",
-      "415 Unsupported Media Type",
-      "416 Requested Range Not Satisfiable",
-      "417 Expectation Failed",
-      "418 I'm a teapot",
-      "421 Misdirected Request",
-      "422 Unprocessable Entity",
-      "423 Locked",
-      "424 Failed Dependency",
-      "426 Upgrade Required",
-      "428 Precondition Required",
-      "429 Too Many Requests",
-      "431 Request Header Fields Too Large",
-      "444 Connection Closed Without Response",
-      "451 Unavailable For Legal Reasons",
-      "499 Client Closed Request",
-      "500 Internal Server Error",
-      "501 Not Implemented",
-      "502 Bad Gateway",
-      "503 Service Unavailable",
-      "504 Gateway Timeout",
-      "505 HTTP Version Not Supported",
-      "506 Variant Also Negotiates",
-      "507 Insufficient Storage",
-      "508 Loop Detected",
-      "510 Not Extended",
-      "511 Network Authentication Required",
-      "599 Network Connect Timeout Error"
-  ]
-  
-    const foundStatus = httpStatuses.filter(httpStatus => httpStatus.startsWith(requestPath));
-  
-    if (requestPath.length == 3 && foundStatus.length > 0) {
-      return new Response(`HTTP ${foundStatus}`,{
-          status: `${requestPath}`,
-      });
-    } if (requestPath.length == 0) {
-      const about = "https://about.statuscodes.org";
-      return Response.redirect(about, 302);
-    } else
-      return new Response("Not an HTTP Status Code... at all.");
+  // Check if the pathname matches the validity of an HTTP Status Code
+  if (requestPath.length === 3 && foundStatus.length > 0) {
+    
+    // Plaintext message for visual confirmation
+    return new Response(`HTTP ${foundStatus}`,{
+        
+        // Return the actual three-digit matching HTTP Status Code
+        status: `${requestPath}`,
+    });
+  } else if (requestPath.length === 0) {
+
+    // Forward naked domain requests to info page
+    return Response.redirect(`https://statuscodes.org/about`, 301);
   }
-  
-  addEventListener("fetch", async event => {
-    event.respondWith(handleRequest(event.request))
-  })
+  else {
+    // A plaintext error message, technically an Error 200 but here we are...
+    return new Response("Unsupported HTTP Status Code");
+}
+}
+addEventListener("fetch", async event => {
+  event.respondWith(handleRequest(event.request))
+})
